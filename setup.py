ee6c6308552d6d75e1d450c0f89cb92a48097de8
Max Bolingbroke <batterseapower@hotmail.com>
2017-12-19 21:38:30 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='crypto-facilities',
+      version='0.1',
+      description='Client for Crypto Facilities REST API',
+      author='Max Bolingbroke',
+      author_email='batterseapower@hotmail.com',
+      url='https://github.com/batterseapower/crypto-facilities',
+      packages=['crypto_facilities'],
+     )
